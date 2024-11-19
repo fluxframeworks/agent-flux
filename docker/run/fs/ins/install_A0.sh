b08869738e96147aa952d495042b30f82ee81336
timthorley <191962010+timthorley@users.noreply.github.com>
2024-11-19 20:46:08 +0100

    git+docker improvements
    version, build branch

--- docker/run/fs/ins/install_A0.sh
+++ docker/run/fs/ins/install_A0.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-BRANCH="development"
+# branch from parameter
+if [ -z "$1" ]; then
+    echo "Error: Branch parameter is empty. Please provide a valid branch name."
+    exit 1
+fi
+BRANCH="$1"
+
 
 git clone -b "$BRANCH" "https://github.com/fluxframeworks/agent-flux" "/git/agent-flux"
+
 
 # Create and activate Python virtual environment
 python3 -m venv /opt/venv
